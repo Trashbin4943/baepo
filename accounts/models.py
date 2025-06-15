c3fba1bf9e596c33ff4571d6e87352ff13ce9ea8
Trashbin4943 <anthonyhwan415@gmail.com>
2025-06-15 12:18:17 +0900

    Feat: models/Customuser update

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     REQUIRED_FIELDS=[]
-    email=None
+    email=models.EmailField(unique=True)
     nickname = models.CharField(max_length=100)
     university = models.CharField(max_length=50)
     location = models.CharField(max_length=200)
