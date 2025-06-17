dde3c3137d5d269104a008bb6a8fb0d6f2671b4b
Trashbin4943 <anthonyhwan415@gmail.com>
2025-06-17 15:19:08 +0900

    commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,8 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-ALLOWED_HOSTS = ['127.0.0.1','43.201.187.33','meotjinsaja.site']
+ALLOWED_HOSTS = ['127.0.0.1','3.38.9.212','luxurylion.site']
 
 
 # Application definition
