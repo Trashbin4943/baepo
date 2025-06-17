b6c54a38b287c4d2daf6c5f54a8ef1d3d5bb5ae8
Trashbin4943 <anthonyhwan415@gmail.com>
2025-06-17 14:50:31 +0900

    초기화

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','43.201.187.33','meotjinsaja.site']
 
