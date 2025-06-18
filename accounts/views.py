60a642d734f115c9f7786a152cd182bbd8120b5c
Trashbin4943 <anthonyhwan415@gmail.com>
2025-06-18 11:23:14 +0900

    debug

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
-from .models import Post
-from .serializers import PostSerializer
+from .models import CustomUser
+from .serializers import *
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
